00c6611d7606338f1b5d6ca8fb3223120fd262e9
Rodrigo Abreu <143100178+rodrigoabr3u@users.noreply.github.com>
2024-05-21 09:52:58 -0300

    att formulario
    Adicionado nav bar, e css no header

---
+++ redirect.php
@@ -0,0 +1,32 @@
+<?php
+    header("refresh: 3;sobre.html");
+    print_r('Pesquisa concluida, obrigado!')
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Redirecionando...</title>
+</head>
+<body>
+<header>
+        <h1>Mentoria</h1>
+    </header>
+    <nav>
+        <a href="sobre.html">Sobre o Emprega//DEV</a>
+        <a href="trilhas.html">Trilhas</a>
+        <a href="dicas.html">Dicas</a>
+        <a href="mentoria.html">Mentorias</a>
+        <a href="formulario.php">Pesquisa</a>
+    </nav>
+    <main>
+    <h1>Pesquisa enviada, obrigado por participar! Por favor aguarde...</h1>
+    </main>
+    <footer>
+        Site desenvolvido para UPX 2
+     </footer>
+</body>
+</html>
